8e64e7ea5c227468ca0ea6eef11f45457a1065a6
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-05-19 10:40:55 -0300

    feat: adiciona função para medir e salvar os tempos de execução dos algoritmos

---
+++ measure_times.jl
@@ -0,0 +1,20 @@
+using DelimitedFiles
+
+function measure_and_save_time(file::String, arr::Vector{T}, f::Function) where T<:Real
+    all_averages = Float64[]
+
+    for i in 100:100:10000
+        times = Float64[]
+        for _ in 1:20
+            start_time = time()
+            f(arr)
+            end_time = time()
+            elapsed = end_time - start_time
+            push!(times, elapsed)
+        end
+        average = sum(times) / length(times)
+        push!(all_averages, average)
+    end
+    
+    writedlm(file, all_averages)
+end
