baa15b6e20cc648affe134f81e5579ee3edaefdb
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-05-20 19:33:15 -0300

    fix: corrige a importação do módulo CreateVector

--- measure_times.jl
+++ measure_times.jl
@@ -3,24 +3,26 @@ module MeasureTimes
 export measure_and_save_time
 
 using DelimitedFiles
+include("create_vector.jl") 
+using .CreateVector
 
-function measure_and_save_time(file::String, arr::Vector{T}, f::Function) where T<:Real
+function measure_and_save_time(file::String; order::Symbol = :random, f::Function)
     all_averages = Float64[]
 
-    for i in 100:100:10000
+    for size in 100:100:30000
         times = Float64[]
+        base_array = create_random_vector(size, order=order)
         for _ in 1:20
+            array = copy(base_array)
             start_time = time()
-            f(arr)
+            f(array)
             end_time = time()
-            elapsed = end_time - start_time
-            push!(times, elapsed)
+            push!(times, end_time - start_time)
         end
-        average = sum(times) / length(times)
-        push!(all_averages, average)
+        push!(all_averages, sum(times) / length(times))
     end
-    
+
     writedlm(file, all_averages)
 end
 
-end #module
+end # module
