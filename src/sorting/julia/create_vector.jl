2b6b4a1cf16c8a822c76376125fb66797d10e172
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-05-27 16:18:53 -0300

    re-add create_vector.jl

---
+++ src/sorting/julia/create_vector.jl
@@ -0,0 +1,21 @@
+module CreateVector
+
+export create_random_vector
+
+using Random
+
+function create_random_vector(n::Int; max::Int=100, order::Symbol=:random, invert::Bool=false)
+    vec = rand(1:max, n)
+
+    if order == :ordered
+        sort!(vec)
+    end
+
+    if invert
+        sort!(vec, rev=true)
+    end
+
+    return vec
+end
+
+end #module
