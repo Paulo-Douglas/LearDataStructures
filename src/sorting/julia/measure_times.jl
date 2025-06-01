8de5094d3bf22a67eb934aa8526c34f35ee79936
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-06-01 08:34:00 -0300

    feat: adiciona parâmetros configuráveis de tamanho, passo e repetições em measure_and_save_time

--- src/sorting/julia/measure_times.jl
+++ src/sorting/julia/measure_times.jl
@@ -2,17 +2,25 @@ module MeasureTimes
 
 export measure_and_save_time
 
-using DelimitedFiles
 include("create_vector.jl") 
 using .CreateVector
 
-function measure_and_save_time(file::String; order::Symbol = :random, f::Function, invert::Bool=false)
+using DelimitedFiles    
+
+function measure_and_save_time(file::String;
+                                order::Symbol = :random,
+                                f::Function,
+                                invert::Bool=false,
+                                step::Int=100,
+                                max_size::Int=1000,
+                                repetitions::Int=1000)
+
     all_averages = Float64[]
 
-    for size in 100:100:1000
+    for size in step:step:max_size
         times = Float64[]
         base_array = create_random_vector(size, order=order, invert=invert)
-        for _ in 1:2000000
+        for _ in 1:repetitions
             array = copy(base_array)
             start_time = time()
             f(array)
